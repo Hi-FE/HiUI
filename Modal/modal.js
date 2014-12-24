a970e8ce9b2e3557ff934c63b2775257983f5006
不一 <buuyii@hitour.cc>
2014-12-24 11:56:36 +0800

    add modal 0.0.2

--- Modal/modal.js
+++ Modal/modal.js
@@ -6,20 +6,26 @@
     function HiModal(options) {
         this.$HiModal = $(options.dom);
         this.id = options.dom;
+        this.default_show = options.default_show ? options.default_show : false;
+        this.bg_type = options.bg_type ? options.bg_type : 'black';
 
         this.init();
-        this.show();
-        this.hide();
+        this.showControl();
+        this.hideControl();
     }
 
     HiModal.prototype.init = function() {
         this.$overlay = $('#' + this.id + ' .full-overlay');
         this.$modal = $('#' + this.id + ' .modal-wrap');
-        console.log(this.$modal);
+
+        if(this.default_show == true) {
+            this.$overlay.fadeIn(200);
+            this.$modal.fadeIn(200);
+        }
 
     };
 
-    HiModal.prototype.show = function() {
+    HiModal.prototype.showControl = function() {
         var self = this;
         $(document).on('click', '*[data-target="' + self.id + '"]', function() {
             self.$overlay.fadeIn(200);
@@ -27,11 +33,11 @@
         });
     };
 
-    HiModal.prototype.hide = function() {
+    HiModal.prototype.hideControl = function() {
         var self = this;
-        self.$overlay.on('click', function() {
+        $(document).on('click', '*[data-close="' + self.id + '"]', function() {
             self.$overlay.fadeOut(200);
-            self.$modal.fadeIn(200);
+            self.$modal.fadeOut(200);
         });
     };
 
